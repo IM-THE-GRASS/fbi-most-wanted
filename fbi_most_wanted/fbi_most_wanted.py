b38b8772f9e507596bcc18ed7b8adea6b8b18e40
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-08-26 13:58:41 -0600

    Revert "AAAAAAAAAAAa"
    This reverts commit 3fe0a28b69d3a25eebf30af6c16535061f02c3f3.

--- fbi_most_wanted/fbi_most_wanted.py
+++ fbi_most_wanted/fbi_most_wanted.py
@@ -1,65 +1,47 @@
 import reflex as rx
 import requests
-import threading
+
 from rxconfig import config
 
 
 class State(rx.State):
     people:list[dict[str,str]] = []
-    def get_people(page=1):
-        response = requests.get(
-            "https://api.fbi.gov/wanted/v1/list", 
-            params={
-                "page":page
-            }          
-        )
-        print(response.text)
+    def get_people(self):
+        response = requests.get("https://api.fbi.gov/wanted/v1/list")
         data = response.json()["items"]
         for person in data:
             new_person = {}
-            if person["title"]:
-                new_person["name"] = person["title"].lower().title()
-            if person["images"]:
-                new_person["img"] = person["images"][0]["large"]
+            new_person["name"] = person["title"]
+            new_person["img"] = person["images"][0]["large"]
             new_person["dsc"] = person["caution"]
-            if person["race"]:
-                new_person["race"] = person["race"].title()
+            new_person["race"] = person["race"]
             new_person["description"] = person["description"]
-            if person["height_min"]:
-                try:
-                    person["height_min"] = str(person["height_min"])
-                    new_person["height"] = person["height_min"][0] + "'" + person["height_min"][2]
-                except:pass 
-            if new_person["dsc"]:
-                return new_person
-    for i in range(5):
-        persons:dict = get_people(i)
-        if persons:
-            people.append(persons)
+            self.people.append(new_person)
+        print(self.people)
+            
 def card(info):
     return rx.center(
-        
         rx.card(
             rx.vstack(
                 rx.image(
                     height="420px",
                     object_fit="cover",
-                    src=info["img"]
+                    src="https://cloud-j01975nem-hack-club-bot.vercel.app/0theguy.png"
                 ),
                 rx.hstack(
-                    # rx.image(
-                    #     src="https://cloud-l3qxq1qyh-hack-club-bot.vercel.app/0male_1.png",
-                    #     width="21px",
-                    #     height="21px",
-                    #     margin_right="11px"
-                    # ),
+                    rx.image(
+                        src="https://cloud-l3qxq1qyh-hack-club-bot.vercel.app/0male_1.png",
+                        width="21px",
+                        height="21px",
+                        margin_right="11px"
+                    ),
                     rx.text(
-                        info["height"],
+                        "5'7",
                         font_size="24px",
                         margin_right="11px"
                     ),
                     rx.text(
-                        info["race"],
+                        "White",
                         font_size="24px",
                         margin_right="11px"
                     ),
@@ -68,39 +50,35 @@ def card(info):
                     margin_top="16px",  
                     padding="2px"
                 ),
-                rx.scroll_area(
-                    rx.vstack(
-                        rx.text(
-                            info["name"],
-                            font_size="24px",
-                            
-                        ),
-                        rx.text(
-                            info["description"],
-                            font_size="16px",
-                            font_weight="bolder",
-                            line_height="22.4px",
-                            margin_top="8px"
-                        ),
-                        rx.html(
-                            info["dsc"],
-                            # font_sise = "14px",
-                            # font_weight="400",
-                            # margin_top="8px"
-                            
-                        ),
-                        
-                        
-                        spacing="0",
-                        margin_top="16px",
-                        
-                        
-                    ),  
+                rx.vstack(
+                    
+                    rx.text(
+                        info["name"],
+                        font_size="24px",
+                          
+                    ),
+                    rx.text(
+                        info["description"],
+                        font_size="16px",
+                        font_weight="bolder",
+                        line_height="22.4px",
+                        margin_top="8px"
+                    ),
+                    rx.text(
+                        "Vasquez-Yanez is a known member of the Mara Salvatrucha (MS-13) gang with ties to El Salvador. He speaks both English and Spanish, although Spanish is his primary language. He was last seen in July of 2016, in Everett, Massachusetts",
+                        font_sise = "14px",
+                        font_weight="400",
+                        margin_top="8px"
+                          
+                    ),
+                    
                     width="100%",
                     height="224px",
-                    margin_top="16px"
+                    spacing="0",
+                    margin_top="16px",
+                    
+                      
                 ),
-                
                 spacing="0"
             ),
             
@@ -115,15 +93,9 @@ def card(info):
         margin_top="16px",
         margin_bottom="16px"
     )
-# @rx.page(on_load=State.on_load)
+@rx.page(on_load=State.get_people)
 def index() -> rx.Component:
     return rx.vstack(
-        # rx.moment(
-        #     format="",
-        #     interval=100,
-        #     on_change=State.on_update,
-        #     display="none"  
-        # ),
         rx.box(
             rx.image(
                 src="https://cloud-6nswpw11z-hack-club-bot.vercel.app/0screenshot_2024-08-23_124044_1.png",
