fefbe76b79b1562defab195b8e1f3cc71f6a78b7
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-08-26 21:45:02 -0600

    fix

--- fbi_most_wanted/fbi_most_wanted.py
+++ fbi_most_wanted/fbi_most_wanted.py
@@ -1,11 +1,10 @@
 import reflex as rx
 import requests
-
 from rxconfig import config
 
-
 class State(rx.State):
-    people:list[dict[str,str]] = []
+    people: list[dict[str, str]] = []
+
     def get_people(self):
         response = requests.get("https://api.fbi.gov/wanted/v1/list")
         data = response.json()["items"]
@@ -18,91 +17,86 @@ class State(rx.State):
             new_person["description"] = person["description"]
             self.people.append(new_person)
         print(self.people)
-            
+
 def card(info):
     return rx.center(
         rx.card(
             rx.vstack(
-                rx.image(
-                    height="420px",
-                    object_fit="cover",
-                    src=info["img"]
+                rx.center(
+                    rx.image(
+                        height="45.7vh",
+                        object_fit="cover",
+                        src=info["img"]
+                    ),
+                    height="45.7vh",
+                    width="20.86vw"
+                    
                 ),
                 rx.vstack(
                     rx.scroll_area(
                         rx.vstack(
-                            
                             rx.text(
                                 info["name"],
-                                font_size="24px",
-                                
+                                font_size="2.5vh",
+                                line_height="2.7vh"
                             ),
                             rx.text(
                                 info["description"],
-                                font_size="16px",
+                                font_size="2.5vh",
                                 font_weight="bolder",
-                                line_height="22.4px",
-                                margin_top="8px"
+                                line_height="3vh",
+                                margin_top="0.87vh"
                             ),
                             rx.html(
                                 info["dsc"],
-                                
                             ),
                             spacing="0",
-                            margin_top="16px",
-                            
+                            margin_top="1.74vh",
                         )
                     ),
-                    
-                    
                     width="100%",
-                    height="224px",
+                    height="24.37vh",
                     spacing="0",
-                    margin_top="16px",
-                    
-                      
+                    margin_top="1.74vh",
                 ),
                 spacing="0"
             ),
-            
-            padding="16px",
-            width="434px",
-            height="734px",
-            
+            padding="1.74vh",
+            width="22.6vw",
+            height="79.87vh",
         ),
-        margin_right="78px",
-        margin_left="78px",
+        margin_right="4.06vw",
+        margin_left="4.06vw",
         height="100%",
-        margin_top="16px",
-        margin_bottom="16px"
+        margin_top="1.74vh",
+        margin_bottom="1.74vh"
     )
+
 @rx.page(on_load=State.get_people)
 def index() -> rx.Component:
     return rx.vstack(
         rx.box(
             rx.image(
                 src="https://cloud-6nswpw11z-hack-club-bot.vercel.app/0screenshot_2024-08-23_124044_1.png",
-                width="1084px",
-                height="187px"
+                width="56.46vw",
+                height="20.35vh"
             ),
             bg="#B62244",
             width="100vw",
-            height="187px"
+            height="20.35vh"
         ),
         rx.grid(
             rx.foreach(
                 State.people,
                 card
             ),
-            margin_top="8px",
+            margin_top="0.87vh",
             columns="3",
             width="100vw",
-            padding_right="78px",
-            
-            padding_left="78px"
+            padding_right="4.06vw",
+            padding_left="4.06vw"
         )
     )
-
 
 app = rx.App()
 app.add_page(index)
